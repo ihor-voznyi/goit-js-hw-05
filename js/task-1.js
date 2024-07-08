2ee9ba79b6afe3debec5045aaf49c5980fff2d9a
ihor-voznyi <haiku.kiev@gmail.com>
2024-07-09 00:54:32 +0300

    upd task1
    added => =>

--- js/task-1.js
+++ js/task-1.js
@@ -1,2 +1,14 @@
 'use strict';
 
+const getUserNames = users => users.map(user => user.name);
+
+console.log(
+    getUserNames([
+        {
+            name: 'Moore Hensley',
+            email: 'moorehensley@indexia.com',
+            balanse: 2811,
+        },
+
+    ])
+);
