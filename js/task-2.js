0ff9845c8640e73774974c3ffaedecc65be46ffe
ihor-voznyi <haiku.kiev@gmail.com>
2024-07-09 01:03:40 +0300

    upd tsk-2
    =>
    const allUsers

--- js/task-2.js
+++ js/task-2.js
@@ -1,2 +1,11 @@
 'use strict';
 
+const getUsersWithFriend = (users, friendName) =>
+    users.filter(user => user.friends.includes(friendName));
+
+const allUsers = [
+{
+    name: 'Moore Hensley',
+    friends: ['Sharron Pace'],
+  },
+];
