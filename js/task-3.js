115f77f4a4a93d6b298bbd340e847e5ec92439ed
ihor-voznyi <haiku.kiev@gmail.com>
2024-07-09 01:11:40 +0300

    upd task3
    =>

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,15 @@
 'use strict';
 
+const sortByDescendingFriendCount = users =>
+    users.toSorted((a, b) => b.friends.length - a.friends.length);
+
+console.log(
+    sortByDescendingFriendCount([
+        {
+            name: 'Moore Hensley',
+            friends: ['Sharron Pace'],
+            gender: 'male',
+        },
+    ])
+);
+
