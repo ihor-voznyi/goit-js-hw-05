8c8da748681ded54ab2ed96ea42a06d18ef649a1
ihor-voznyi <haiku.kiev@gmail.com>
2024-07-09 01:23:22 +0300

    upd task4 init
    =>

--- js/task-4.js
+++ js/task-4.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const getTotalBalanceByGender = (users, gender) =>
+    users
+        .filter(user => user.gender === gender)
+        .reduce((perviousValue, currentValue) => {
+    return perviousValue + currentValue.balance;
+        }, 0);
+
+const clients = [
+            {
+    name: 'Moore Hensley',
+    gender: 'male',
+    balance: 2811,
+  },
+];
